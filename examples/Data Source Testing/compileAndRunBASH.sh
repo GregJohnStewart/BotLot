b0a001956961634a2babce349e14449f477a1c8c
Greg Stewart <contact@gjstewart.net>
2016-05-06 12:31:54 -0400

    Fixed compile and run for bash

--- examples/Data Source Testing/compileAndRunBASH.sh
+++ examples/Data Source Testing/compileAndRunBASH.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Compiles and runs DSTest.java
 
-javac -classpath "..\..\dist\BotLot.jar" DSTest.java
+javac -classpath "../../dist/BotLot.jar" DSTest.java
 
-java -classpath "..\..\dist\BotLot.jar:." DSTest
+java -cp ../../dist/BotLot.jar:. DSTest
